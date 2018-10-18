79252f215dbe08f580dc6e7638cebbe0c34c1e43
nbokulich <nbokulich@gmail.com>
2018-10-18 16:10:26 -0500

    versioneer setup

--- q2_shogun/__init__.py
+++ q2_shogun/__init__.py
@@ -11,3 +11,7 @@ from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
