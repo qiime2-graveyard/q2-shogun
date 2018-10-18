335e9be8e44b9dcd0397f3cfae7644b2a72012fe
nbokulich <nbokulich@gmail.com>
2018-10-18 13:41:52 -0500

    initializing

---
+++ setup.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2018, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name="q2-shogun",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    author="Nicholas Bokulich",
+    author_email="nbokulich@gmail.com",
+    description="Shallow shotgun sequencing taxonomy profiler",
+    license='BSD-3-Clause',
+    url="https://qiime2.org",
+    entry_points={
+        'qiime2.plugins':
+        ['q2-shogun=q2_shogun.plugin_setup:plugin']
+    },
+    package_data={
+        'q2_shogun': ['citations.bib'],
+        'q2_shogun.tests': ['data/*'],
+    },
+    zip_safe=False,
+)
